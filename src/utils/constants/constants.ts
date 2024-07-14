ffd2156d3c7db58fb85f3263d6ee543d3c3f8641
abhishek <abhishekchorotiya@openinapp.com>
2024-07-14 20:31:38 +0530

    update

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,81 +1,81 @@
 export const SKILLS = [
-    {
-        name: "Next.js",
-        icon: "/nextjs.svg",
-        bg: true
-    },
-    {
-        name: "React.js",
-        icon: "/reactjs.svg",
-        bg: false
-    },
-    {
-        name: "JavaScript",
-        icon: "/js.svg",
-        bg: false
-    },
-    {
-        name: "TypeScript",
-        icon: "/ts.svg",
-        bg: false
-    },
-    {
-        name: "Node.js",
-        icon: "/nodejs.svg",
-        bg: false
-    },
-    {
-        name: "Tailwind",
-        icon: "/tailwind.svg",
-        bg: false
-    },
-    {
-        name: "Git",
-        icon: "/git.svg",
-        bg: false
-    },
-    {
-        name: "GitHub",
-        icon: "/github.svg",
-        bg: true
-    },
-    {
-        name: "React Query",
-        icon: "/reactquery.svg",
-        bg: false
-    },
-    {
-        name: "MongoDB",
-        icon: "/mongodb.svg",
-        bg: false
-    },
-    {
-        name:"AWS",
-        icon:"/aws.svg",
-        bg: false
-    },
-]
+  {
+    name: "Next.js",
+    icon: "/nextjs.svg",
+    bg: true,
+  },
+  {
+    name: "React.js",
+    icon: "/reactjs.svg",
+    bg: false,
+  },
+  {
+    name: "JavaScript",
+    icon: "/js.svg",
+    bg: false,
+  },
+  {
+    name: "TypeScript",
+    icon: "/ts.svg",
+    bg: false,
+  },
+  {
+    name: "Node.js",
+    icon: "/nodejs.svg",
+    bg: false,
+  },
+  {
+    name: "Tailwind",
+    icon: "/tailwind.svg",
+    bg: false,
+  },
+  {
+    name: "Git",
+    icon: "/git.svg",
+    bg: false,
+  },
+  {
+    name: "GitHub",
+    icon: "/github.svg",
+    bg: true,
+  },
+  {
+    name: "React Query",
+    icon: "/reactquery.svg",
+    bg: false,
+  },
+  {
+    name: "MongoDB",
+    icon: "/mongodb.svg",
+    bg: false,
+  },
+  {
+    name: "AWS",
+    icon: "/aws.svg",
+    bg: false,
+  },
+];
 
 export const SOCIALS = [
-    {
-        name: "LinkedIn",
-        icon: "/linkedinn.svg",
-        link: "https://www.linkedin.com/in/abhishek-chorotiya-0b9b01223/"
-    },
+  {
+    name: "LinkedIn",
+    icon: "/linkedinn.svg",
+    link: "https://www.linkedin.com/in/abhishek-chorotiya-0b9b01223/",
+  },
 
-    {
-        name: "Github",
-        icon: "/github.svg",
-        link: "https://github.com/abhishekchorotya"
-    },
-    {
-        name: "LeetCode",
-        icon: "/leetcode.png",
-        link: "https://twitter.com/abhishekchorotya"
-    },
-    {
-        name: "Instagram",
-        icon: "/instagram.svg",
-        link: "https://twitter.com/abhishekchorotya"
-    },
-]
+  {
+    name: "Github",
+    icon: "/github.svg",
+    link: "https://github.com/abhishekchorotya",
+  },
+  {
+    name: "LeetCode",
+    icon: "/leetcode.png",
+    link: "https://leetcode.com/u/Abhishek_Chorotiya/",
+  },
+  {
+    name: "Instagram",
+    icon: "/instagram.svg",
+    link: "https://www.instagram.com/abhishekchorotiya",
+  },
+];
