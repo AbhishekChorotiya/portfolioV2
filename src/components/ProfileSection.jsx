e8a14b4b2cfd58d81de8400d7cbb8cd4cf80ba9e
abhishek <abhishekchorotiya@openinapp.com>
2024-07-15 18:30:20 +0530

    removed lazy loading on landing page

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -8,13 +8,18 @@ const ProfileSection = () => {
   return (
     <div className="w-full fixed top-0 left-0 max-w-[450px] bg-background lg:flex min-h-dvh lg:relative">
       <div className="absolute top-0 left-0 w-full h-1/2 ">
-        <Image src="/bgGroup.svg" layout="fill" alt="bg" />
+        <Image loading="eager" src="/bgGroup.svg" layout="fill" alt="bg" />
         <div className="absolute bottom-0 h-full w-full bg-gradient-to-t from-background" />
         <div className="absolute bottom-0 h-2/5 w-full bg-gradient-to-t from-background/50" />
       </div>
       <div className="absolute flex flex-col items-center top-0 left-0 w-full h-full pt-16 px-[15%]">
         <div className="w-3/5 mb-10 aspect-square relative rounded-full border-[3px] border-white/20 shadow-profile">
-          <Image alt="photo" src={"/photoCircle.png"} layout="fill" />
+          <Image
+            loading="eager"
+            alt="photo"
+            src={"/photoCircle.png"}
+            layout="fill"
+          />
         </div>
         <h1 className="text-3xl mb-4 text-center w-full font-livvic text-textPrimary font-semibold">
           Abhishek Chorotiya
@@ -34,6 +39,7 @@ const ProfileSection = () => {
               <Image
                 src={social.icon || ""}
                 alt="linkedin"
+                loading="eager"
                 className={`${social.name === "LinkedIn" && "scale-125"}`}
                 layout="fill"
               />
@@ -45,7 +51,7 @@ const ProfileSection = () => {
             href="https://drive.google.com/file/d/1h150C_6F7fOyt3Sn8mF2Fn-8x1Tqkl5B/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
-            className="h-fit px-5 border-2 text-sm border-textSecondary/20 py-2 rounded-full text-textSecondary"
+            className="h-fit px-5 border-2 text-sm border-textPrimary py-2 rounded-full text-textPrimary"
           >
             Resume
           </a>
@@ -55,7 +61,7 @@ const ProfileSection = () => {
                 behavior: "smooth",
               });
             }}
-            className="px-5 h-fit border-2 text-sm border-textSecondary/20 py-2 rounded-full text-textSecondary"
+            className="px-5 h-fit border-2 text-sm border-textPrimary py-2 rounded-full text-textPrimary"
           >
             Hire Me!
           </button>
