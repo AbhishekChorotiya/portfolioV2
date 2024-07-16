ea45d5526c6d94d21116c6af7cf478b1d1e0de3a
abhishek <abhishekchorotiya@openinapp.com>
2024-07-16 08:37:47 +0530

    font optimizaton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=Livvic:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
         <script
           async
           defer
