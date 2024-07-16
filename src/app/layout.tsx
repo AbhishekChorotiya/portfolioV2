a919ed9d7162608479f524a65a5bb932cb2ece39
abhishek <abhishekchorotiya@openinapp.com>
2024-07-16 09:17:34 +0530

    performance optimizaton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -41,19 +42,13 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
           href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=Livvic:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap"
           rel="stylesheet"
         />
-        <script
-          async
-          defer
+        <Script
           src="https://scripts.simpleanalyticscdn.com/latest.js"
-        ></script>
+          strategy="afterInteractive"
+        />
         <noscript>
           <img
             src="https://queue.simpleanalyticscdn.com/noscript.gif"
