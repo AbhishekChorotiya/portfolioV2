3b41a4eed621fddda2e204ae7c8ec992922204eb
Abhishek chorotiya <82277533+AbhishekChorotiya@users.noreply.github.com>
2024-11-10 04:09:09 +0530

    update: removed TTS

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -60,22 +60,23 @@ export async function POST(request: NextRequest) {
 
     const result = await chatSession?.sendMessage(prompt);
 
-    const client = new TextToSpeechClient({
-      credentials: JSON.parse(credentials),
-    });
+    // const client = new TextToSpeechClient({
+    //   credentials: JSON.parse(credentials),
+    // });
 
-    const requestConfig = {
-      input: { text: result?.response?.text() },
-      voice: {
-        languageCode: "hi-IN",
-        name: "hi-IN-Neural2-C",
-        ssmlGender: "MALE",
-      },
-      audioConfig: { audioEncoding: "MP3" },
-    };
+    // const requestConfig = {
+    //   input: { text: result?.response?.text() },
+    //   voice: {
+    //     languageCode: "hi-IN",
+    //     name: "hi-IN-Neural2-C",
+    //     ssmlGender: "MALE",
+    //   },
+    //   audioConfig: { audioEncoding: "MP3" },
+    // };
 
     //@ts-ignore
-    const [response] = await client?.synthesizeSpeech(requestConfig);
+    // const [response] = await client?.synthesizeSpeech(requestConfig);
+    const response = []
 
     return NextResponse.json({
       result: result?.response?.text(),
